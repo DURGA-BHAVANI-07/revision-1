ff0a29aac6b1e96c75292eb3f2545188a3033331
K.Durga Bhavani <bhavanidurga759@gmail.com>
2024-10-12 17:18:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// 
+
+// var a=10;
+// var b=20;
+// console.log(a+b)
+// console.log(10 || 20)
+
+// var choose="projects"
+// switch(choose){
+//    case "Home" :
+//     console.log("home")
+//     break;
+//     case "Assignements":
+//         console.log("assignment")
+//         break;
+//       case "Mocks":
+//         console.log("mocks")
+//         break;
+
+//     default:
+//         console.log("choose one case")
+// }
+
+// var user1=""
+// var user2=""
+// var choose="projects"
+// switch(choose){
+//    case "Home" :
+//     console.log("home")
+//     break;
+//     case "Assignements":
+//         console.log("assignment")
+//         break;
+//       case "Mocks":
+//         console.log("mocks")
+//         break;
+
+//     default:
+//         console.log("choose one case")
+// }
+
+// if(user1){
+//     switch()
+// }else if(user2){
+
+// }else{
+//     console.log("user 3")
+// }
+
+
+// setInterval(function(){
+//     console.log("hello")
+// },5000)
+
+var a=[10,100,200];
+a.push("vamsi",null,{id:1},"bhavani",undefined)
+a.lenth=5
+a.unshift("hello","ravi")
+a.shift()
+a.shift()
+a.shift()
+a.shift()
+console.log(a)
+
